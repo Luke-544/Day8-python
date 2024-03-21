2c15bb28d79c4ef96432c5320282c4763866034d
Luke-544 <lukesamuels422@gmail.com>
2024-03-21 20:35:58 +0300

    Create day8.py

---
+++ day8.py
@@ -0,0 +1,26 @@
+#lists in python - variable that can store multiple values
+
+food = ["Ugali","Githeri","Chapati","Rice"]
+
+#print(food[0])
+
+#you can change the value of an item in the list
+#food[1] = "beans"
+#print(food[1])
+
+#you can be able to show the entire list
+
+#for x in food:
+   # print(x)
+
+#there are various functions that are used with lists
+
+#food.sort()
+#food.append("peas")
+#food.insert(0,"beans")
+#food.remove("Chapati")
+#food.pop()
+#food.clear()
+
+for x in food:
+    print(x)
